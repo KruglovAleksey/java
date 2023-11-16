18eef15be255eac7da3f0b0e4314ccde3dd67edc
aleksey <razdolbam@yandex.ru>
2023-11-16 18:36:41 +0300

    git init

---
+++ program.java
@@ -0,0 +1,26 @@
+package phoneBook;
+
+public class program {
+    public static void main(String[] args) {
+        Tell tell = new Tell();
+        tell.add("Vasya", "89525122270");
+        tell.add("Vasya", "89525122271");
+        tell.add("Petiy", "89525122274");
+        tell.add("Petiy", "89525122290");
+        tell.add("Petiy", "89525122231");
+        tell.add("Petiy", "89525122279");
+        tell.add("Vova", "89525121279");
+        tell.add("Vova", "89525121279");
+        tell.add("Vova", "89525121279");
+        //System.out.println(tell);
+        //System.out.println(tell.find("Vasya"));        
+        //System.out.println(tell.dell("Vova"));
+        //System.out.println(tell);
+        //tell.edit("Vasya", "89525122270", "89525122255");
+        //System.out.println(tell);
+        tell.print();
+
+    }
+}
+
+
